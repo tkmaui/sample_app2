8880a72f7970845bba81d27d84e31991ef43567a
EC2 Default User <ec2-user@ip-172-31-7-216.ap-northeast-1.compute.internal>
2023-05-15 09:56:05 +0000

    Add3章

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,21 @@
 class ListsController < ApplicationController
   def new
+    @list = List.new
   end
 
   def index
+    @lists - List.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
