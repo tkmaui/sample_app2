e06053bd4ca8b4aee141a7262c1052b003138e26
EC2 Default User <ec2-user@ip-172-31-7-216.ap-northeast-1.compute.internal>
2023-05-16 01:42:54 +0000

    add5章

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,17 +4,31 @@ class ListsController < ApplicationController
   end
 
   def index
-    @lists - List.all
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
+  end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to list_path(list.id)
   end
 
   def edit
+    @list = List.find(params[:id])
   end
-  
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
   private
-  
+
   def list_params
     params.require(:list).permit(:title, :body)
   end
